b8f87393b8218a11fc79f6a0cfb04f6a5f269b6e
Edward-K1 <eddyk1collab@gmail.com>
2021-09-29 06:04:45 +0300

    enable getting of function data in decorator

--- validatedata.py
+++ validatedata.py
@@ -1,4 +1,7 @@
 from functools import wraps
+from inspect import getfullargspec
+
+from collections import OrderedDict
 
 from .validator import Validator
 
@@ -6,13 +9,48 @@ _BASIC_TYPES = ('bool', 'date', 'email', 'even', 'float', 'int', 'odd', 'str')
 _EXTENDED_TYPES = ('dict', 'list', 'regex', 'set', 'tuple')
 
 
-def validate(rule, message='', data=':*params:', raise_exceptions=False):
+def validate(rule, message='', raise_exceptions=False, is_class=False):
     def decorator(func):
         @wraps(func)
-        def wrapper(*args, **kwargs):
-            result = validate_data(data, rule, message, raise_exceptions, func)
-            if result.ok:
-                return func(*args, **kwargs)
+        def wrapper(obj, *args, **kwargs):
+            func_data = OrderedDict()
+            func_defn = getfullargspec(func)
+            obj_is_cls = True if (is_class == True
+                                  or func_defn.args[0] == 'self') else False
+            clean_params = func_defn.args[1:] if obj_is_cls else func_defn.args
+
+            # set initial value of keys to empty strings
+            func_data.update(
+                zip(clean_params, ['' for x in range(len(clean_params))]))
+
+            # assign default values to keys that had them
+            if func_defn.defaults:
+                defaults_dict = OrderedDict(
+                    zip(clean_params[-len(func_defn.defaults):],
+                        func_defn.defaults))
+                func_data.update(defaults_dict)
+                func_data['default_values'] = defaults_dict
+
+            # if obj is not a class, it contains the value of the first parameter
+            if not obj_is_cls:
+                func_data[clean_params[0]] = obj
+
+            if args:
+                if obj_is_cls:
+                    func_data.update(zip(clean_params, args))
+                else:
+                    func_data.update(zip(clean_params[1:], args))
+
+            if kwargs:
+                func_data.update(
+                    zip([
+                        k for k in kwargs.keys() if k in set(func_data.keys())
+                    ], kwargs.values()))
+
+            result = validate_data(func_data, rule, message, raise_exceptions)
+
+            if result['ok']:
+                return func(obj, *args, **kwargs)
             else:
                 return {'errors': result.errors}
 
@@ -21,7 +59,7 @@ def validate(rule, message='', data=':*params:', raise_exceptions=False):
     return decorator
 
 
-def validate_data(data, rule, message='', raise_exceptions=False, func=None):
+def validate_data(data, rule, message='', raise_exceptions=False, **kwargs):
     errors = []
     result = {'ok': False}
 
@@ -59,30 +97,27 @@ def expand_rule(rule):
             'Validation rule must be of type: str, list, tuple, or dict')
 
     if len(str(rule)) < 3:
-        raise ValueError('Invalid rule')
-
-
-
+        raise ValueError('Invalid rule length')
 
     def expand_rule_string(rule):
         rule_dict = {}
-        _type = rule.split(':')[0] if ':' in rule else rule
+        _type = rule.split(':')[0].strip() if ':' in rule else rule
 
-        if _type not in _BASIC_TYPES + _EXTENDED_TYPES:
+        if _type not in set(_BASIC_TYPES + _EXTENDED_TYPES):
             raise TypeError(f'{_type} is not supported')
 
-        message = rule.split(':msg:')[1] if ':msg:' in rule else ''
-        without_msg = rule.split(':msg:')[0] if message else rule
+        msg = rule.split(':msg:')[1] if ':msg:' in rule else ''
+        without_msg = rule.split(':msg:')[0] if msg else rule
         to_range = without_msg.split(':')[-3], without_msg.split(
             ':')[-1] if ':to:' in without_msg else ''
 
-        rule_dict['type'], rule_dict['message'] = _type, message
+        rule_dict['type'], rule_dict['message'] = _type, msg
 
         if to_range:
             rule_dict['range'] = (to_range[0], to_range[1])
 
-        if _type in ('int', 'float'):
-            rule_dict['strict'] = True if ':strict:' in rule else False
+        if _type in {'int', 'float'}:
+            rule_dict['strict'] = True if ':strict' in rule else False
 
         if _type == 'regex':
             if len(rule.split(':')) < 2:
@@ -92,12 +127,10 @@ def expand_rule(rule):
 
         if len(rule.split(':')) == 2:
             length = rule.split(':')[1]
-            if _type not in ('regex', 'float', 'date') and length.isdigit():
+            if _type not in {'regex', 'float', 'date'} and length.isdigit():
                 rule_dict['length'] = int(length)
 
         return rule_dict
-
-
 
     if isinstance(rule, str):
         expanded_rules = expand_rule_string(rule)
