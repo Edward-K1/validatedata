7517047c81255d18210d6f831392b13657b09eff
Edward <38511070+Edward-K1@users.noreply.github.com>
2026-02-27 13:31:07 +0300

    Fix workflow release (#8)
    * expand custom types
    
    * fix failing string test
    
    * test new features
    
    * remove python 3.7 from github workflow file
    
    * fix pypi upload

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 import setuptools
+import re
 
-from validatedata import __version__
+# Read version without importing the package
+with open("validatedata/__init__.py", "r") as f:
+    version_file = f.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        __version__ = version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -18,12 +27,11 @@ setuptools.setup(
     install_requires=['python-dateutil'],
     packages=setuptools.find_packages(exclude=['tests', '.github']),
     include_package_data=True,
-    keywords = "validate data validation",
+    keywords="validate data validation",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
-
 )
