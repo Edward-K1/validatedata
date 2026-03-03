49354d44ff7bf517f3c673b992ef2f64fa6a44e1
Edward <38511070+Edward-K1@users.noreply.github.com>
2026-03-03 22:57:06 +0300

    chore: bump version to 0.3.0 (#16)

--- validatedata/__init__.py
+++ validatedata/__init__.py
@@ -1,7 +1,7 @@
 from .validatedata import validate, validate_data, validate_types, ValidationResult
 from .validator import ValidationError
 
-__version__ = '0.3'
+__version__ = '0.3.0'
 
 __all__ = [
     'validate',
